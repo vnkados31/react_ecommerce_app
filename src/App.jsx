e33044de187a550d61680b02b955136164b0647a
Vaibhav Kados <vaibhavkados@gmail.com>
2024-10-12 15:59:36 +0530

    Redux setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { increment } from './features/counter/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const count1 = useSelector((state)=> state.counter.value)
+  const dispatch = useDispatch()
+
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,8 +24,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          count is {count1}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
+import store from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <Provider store={store}>
+     {/* <StrictMode> */}
+        <App />
+     {/* </StrictMode>, */}
+  </Provider>
+  
 )
